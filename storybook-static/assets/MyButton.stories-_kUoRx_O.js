7bcbb767f435f5abb6cd2ed9c0ebb25645418559
vicky <kumarvicky6565@gmail.com>
2024-09-09 12:21:33 +0530

    second commit

---
+++ storybook-static/assets/MyButton.stories-_kUoRx_O.js
@@ -0,0 +1,21 @@
+import{d as _,o as g,b as f,e as M}from"./vue.esm-bundler-BaqNLAmL.js";const i=_({name:"MyButton"}),x=(t,n)=>{const s=t.__vccOpts||t;for(const[r,a]of n)s[r]=a;return s},v={class:"my-button"};function b(t,n,s,r,a,T){return g(),f("button",v,[M(t.$slots,"default",{},void 0,!0)])}const B=x(i,[["render",b],["__scopeId","data-v-6c4af6d9"]]);i.__docgenInfo={displayName:"MyButton",exportName:"default",description:"",tags:{},slots:[{name:"default"}],sourceFiles:["C:/Users/kumar/OneDrive/Desktop/modules/test 1/src/components/MyButton.vue"]};const $={title:"Example/MyButton",component:B,tags:["autodocs"],argTypes:{}},y=t=>({components:{MyButton:B},setup(){return{args:t}},template:'<MyButton v-bind="args">Button Text</MyButton>'}),e=y.bind({});e.args={};const o=y.bind({});o.args={};var u,c,p;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`args => ({
+  components: {
+    MyButton
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: '<MyButton v-bind="args">Button Text</MyButton>'
+})`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,d,l;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`args => ({
+  components: {
+    MyButton
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: '<MyButton v-bind="args">Button Text</MyButton>'
+})`,...(l=(d=o.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const h=["Default","WithCustomText"];export{e as Default,o as WithCustomText,h as __namedExportsOrder,$ as default};
